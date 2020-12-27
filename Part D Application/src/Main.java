e164e23e77c94e897d9d7120c06f5912e9229580
Craftmonger <afrikaans777@hotmail.com>
2020-12-27 17:29:40 +1300

    Start of Application

--- Part D Application/src/Main.java
+++ Part D Application/src/Main.java
@@ -1,5 +1,16 @@
 
 public class Main 
 {
-
+	public String name;
+	public String colour;
+	public String transportation;
+	public String day;
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi my name is________");
+		System.out.println("My favourite colour is________");
+		System.out.println("My favourite mode of transportation is________");
+		System.out.println("The best day of the week is________");
+	}
 }
