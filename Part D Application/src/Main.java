0f952aea0ea5f5448836f615f3e926ed145a5d97
Craftmonger <afrikaans777@hotmail.com>
2020-12-27 17:37:19 +1300

    Added user input

--- Part D Application/src/Main.java
+++ Part D Application/src/Main.java
@@ -1,16 +1,21 @@
-
+import java.util.Scanner;
 public class Main 
 {
-	public String name;
-	public String colour;
-	public String transportation;
-	public String day;
+	public static Scanner scanner = new Scanner(System.in);	
+	public static String name;
+	public static String colour;
+	public static String transportation;
+	public static String day;
 	
 	public static void main(String[] args) 
 	{
 		System.out.println("Hi my name is________");
+		name = scanner.nextLine(); 
 		System.out.println("My favourite colour is________");
+		colour = scanner.nextLine(); 
 		System.out.println("My favourite mode of transportation is________");
+		transportation = scanner.nextLine(); 
 		System.out.println("The best day of the week is________");
+		day = scanner.nextLine(); 
 	}
 }
